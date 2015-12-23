aae692abe161b13b7a0d766cb50c1838b35a7420
uNkNowN92 <christianjohnsumampong@gmail.com>
2015-12-23 15:32:59 +0800

    Added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,41 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
-var jshint     = require('gulp-jshint');
-var concat     = require('gulp-concat');
-var uglify     = require('gulp-uglify');
-var rename     = require('gulp-rename');
-var header     = require('gulp-header');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
 
-var pkg = 'package.json';
 var scripts = 'src/*.js';
 var filename = 'knockout-paged-list.js';
 
-var banner = '/* Knockout Paged List <%= pkg.version %> */\n';
-banner += '/* <%= pkg.homepage %> */\n';
-banner += '/* by <%= pkg.author %> */\n';
-
+// using data from package.json 
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
+  
 gulp.task('build-script', function () {
-  return gulp.src(scripts)
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest(''))
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(uglify())
-    .pipe(gulp.dest(''));
+    return gulp.src(scripts)
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(gulp.dest(''))
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(uglify())
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(gulp.dest(''));
 });
 
 gulp.task('lint', function () {
-  return gulp.src(scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    return gulp.src(scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(scripts, ['lint', 'build-script']);
 });
 
