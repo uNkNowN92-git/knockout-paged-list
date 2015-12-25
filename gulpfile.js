12d2f8a5b91eeacf4cd6563f17123f79d739af52
uNkNowN92 <christianjohnsumampong@gmail.com>
2015-12-25 19:00:15 +0800

    Update build destination and filename

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,30 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
 
-var scripts = 'src/*.js';
-var filename = 'knockout-paged-list.js';
-
 // using data from package.json 
 var pkg = require('./package.json');
-var banner = ['/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
+var banner = ['/*',
+  ' * <%= pkg.name %> v<%= pkg.version %>',
+  ' * <%= pkg.description %>',
   ' * @repository <%= pkg.repository.url %>',
   ' * @license <%= pkg.license %>',
   ' */',
   ''].join('\n');
   
+var scripts = 'src/*.js';
+var filename = 'knockout-paged-list' + pkg.version;
+var buildDestination = 'build/' + pkg.version;
+
 gulp.task('build', function () {
     return gulp.src(scripts)
         .pipe(header(banner, { pkg : pkg } ))
+        .pipe(rename({ extname: '-' + pkg.version + '.js' }))
+        .pipe(gulp.dest(buildDestination))
         .pipe(gulp.dest(''))
         .pipe(rename({ extname: '.min.js' }))
         .pipe(uglify())
         .pipe(header(banner, { pkg : pkg } ))
+        .pipe(gulp.dest(buildDestination))
         .pipe(gulp.dest(''));
 });
 
